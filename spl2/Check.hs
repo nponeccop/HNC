143af34f10174091ca63c27ab15e6c674667b30b
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2007-12-23 10:45:51 +0000

    1
    git-svn-id: http://inv.googlecode.com/svn/trunk@69 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/Check.hs
+++ spl2/Check.hs
@@ -1,5 +1,16 @@
 
-module Check where
+module Check (P (..), check, res) where
+
+import Code hiding (res)
+
+data P = P Bool | N
+
+check (CNum n) = P True
+check (CBool n) = P True
+check (CVal n) = P True
+check (CStr n) = P True
+--check (CL n) = P True
+check o = error (show o)
 
 res = "1"
 
--- spl2/Interpretator.hs
+++ spl2/Interpretator.hs
@@ -3,13 +3,17 @@ module Interpretator where
 
 import Parser
 import Compiler
+import Check
 import Code
 
 step str =
 	case parse str of
 	Parser.P i p ->
 		case compile p of
-		Compiler.P c -> show (eval c base)
+		Compiler.P c ->
+			case check c of
+			Check.P c2 -> show (eval c base)
+			Check.N -> "check error"
 		Compiler.N -> "compile error"
 	Parser.N -> "parser error"
 
