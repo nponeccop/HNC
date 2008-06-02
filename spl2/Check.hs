ac2b3026b2f5bec9f451726813c79c7c04953004
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-06-02 22:40:41 +0000

    dep type in progress
    git-svn-id: http://inv.googlecode.com/svn/trunk@87 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/Check.hs
+++ spl2/Check.hs
@@ -11,13 +11,14 @@ data T =
 	T [Char]
 	| TT [T]
 	| TU
+	| TD [Char] T
 	deriving (Eq, Show)
 
 base = M.fromList $
 	("sum", TT [T "num", T "num", T "num"]):
 	("list", TT [T "num", T "list"]):
-	("joina", TT [T "num", T "list", T "list"]):
-	("elist", T "list"):
+	("joina", TT [TU, TD "list" TU, TD "list" TU]):
+	("elist", TD "list" TU):
 	("length", TT [T "list", T "num"]):
 	("to_string", TT [T "num", T "string"]):
 	[]
@@ -26,45 +27,53 @@ is_val (CVal n) = True
 is_val o = False
 get_val_name (CVal n) = n
 
-check (CNum n) e et = P ([], T "num")
-check (CBool n) e et = P ([], T "bool")
-check (CStr n) e et = P ([], T "str")
-check (CVal n) e et =
+check (CNum n) e et u = P ([], T "num")
+check (CBool n) e et u = P ([], T "bool")
+check (CStr n) e et u = P ([], T "str")
+check (CVal n) e et u =
 	case M.lookup n et of
 		Just a -> P ([], a)
 		Nothing -> N $ (++) "check cannot find " $ show n
 
-check (CL a (K p)) e et =
-	case check a e et of
-		P (_, TT p1) ->
-			ch p1 p e et
+check (CL a (K p)) e et u =
+	case check a e et u of
+		P ([], TT p1) ->
+			ch p1 p e et u
 			where
-				ch p1 p2 e et =
+				ch p1 p2 e et u =
 					case (p1, p2) of
 						((p1:p1s), (p2:p2s)) ->
-							case (check p2 e et, ch p1s p2s e et) of
-								(P (u, r), P (u2, r2))| p1 == r ->
-									P $ (,) (u ++ u2) $ r2
-								(P (u, r), P (u2, r2))| is_val p2 && TU == r ->
+							case (check p2 e et u, ch p1s p2s e et u) of
+								(P (u1, r), P (u2, r2))| p1 == r ->
+									P $ (,) (u1 ++ u2) $ r2
+								(P (u1, r), P (u2, r2))| is_val p2 && TU == r ->
 									P $ (,) ((get_val_name p2, p1):u2) $ r2
-								(P (u, r), o2) ->
-									N $ "expected "++(show p1)++" actual "++(show r)
+								(P (u1, r), P (u2, r2))| TU == p1 ->
+									ch p1s p2s e et (r:u)
+								(P (u1, r), o2) ->
+									N $ "expected "++(show $ setu p1 u)++" actual "++(show r)
 								(o, o2) -> o
-						(r:[], []) -> P ([], r)
-						(r, []) ->  P ([], TT r)
+						(r:[], []) -> P ([], setu r u)
+						(r, []) ->  P ([], setu (TT r) u)
 		P (_, _) -> N "err1"
 		o -> o
 
-check (CL a (S p)) e et =
-	case check a e (putp p (take (length p) $ repeat TU) et) of
+check (CL a (S p)) e et u =
+	case check a e (putp p (take (length p) $ repeat TU) et) u of
 		P (us, ts) -> P ([], TT $ (map (\(n, t) -> t) us)++[ts])
 		o -> o
 
 putp (v:vs) (c:cs) et = putp vs cs (M.insert v c et)
 putp [] [] et = et
 
+setu (TD n t) u = TD n (setu t u)
+setu (TT tt) u = TT (map (\t -> setu t u) tt)
+setu TU (t2:t2s) = t2
+setu o (t2:t2s) = o
+setu o [] = o
+
 check_all o =
-	check o Code.base Check.base
+	check o Code.base Check.base []
 
 res = "1"
 
--- spl2/Test.hs
+++ spl2/Test.hs
@@ -11,23 +11,27 @@ test (s, r) =
 		Interpretator.P (t, r2)| r == r2 ->
 			"ok: str: " ++ s ++ "\n  type: " ++ t ++ "\n  res: " ++ r2
 		Interpretator.P (t, r2) ->
-			"no: str: " ++ s ++ "\n  exp: " ++ r ++ "\n  res: " ++ r2
+			"no: str: " ++ s ++ "\n  type: " ++ t ++ "\n  exp: " ++ r ++ "\n  res: " ++ r2
 		Interpretator.N r3 ->
 			("no: str: "++s++"\n  res: "++r3++"\n  exp: "++r)
 
 tests = [
-	("1", "CNum 1")
-	,("12", "CNum 12")
-	,("1b", "CBool True")
-	,("sum", "CL (CInFun \"sum\" 2 InFun) (K [])")
-	,("'abc'", "CStr \"abc\"")
-	,("sum 'abc' 2", "CStr \"abc\"")
-	,("sum 2", "CStr \"abc\"")
-	,("(sum 1)", "CL (CInFun 2 InFun) (K [CNum 1])")
-	,("to_string,sum 2,length,joina 9,joina 8,elist", "CNum 4")
-	,("(sum _,sum 1 2*_)", "")
-	,("(sum 1,sum _ 2*_)", "")
-	,("(sum 1,sum ((sum 3,length z*z) _) 2*_)", "")
+	("elist", "")
+	,("joina 1,elist", "")
+	,("joina 1", "")
+	,("joina 1,2", "")
+--	("1", "CNum 1")
+--	,("12", "CNum 12")
+--	,("1b", "CBool True")
+--	,("sum", "CL (CInFun \"sum\" 2 InFun) (K [])")
+--	,("'abc'", "CStr \"abc\"")
+--	,("sum 'abc' 2", "CStr \"abc\"")
+--	,("sum 2", "CStr \"abc\"")
+--	,("(sum 1)", "CL (CInFun 2 InFun) (K [CNum 1])")
+--	,("to_string,sum 2,length,joina 9,joina 8,elist", "CNum 4")
+--	,("(sum _,sum 1 2*_)", "")
+--	,("(sum 1,sum _ 2*_)", "")
+--	,("(sum 1,sum ((sum 3,length z*z) _) 2*_)", "")
 --	,("sum 1,mul 4 2", "CNum 9")
 --	,("(_z 1*_z) (if (less _ 5) (sum _,_f,sum _ 1!l) (_!l)*_!r)", "CNum 15")
 --	,("(_,list 8 9 4 4 5 3*_) (if (is_empty _) (_!l) ((join (_f,filter (not,less h _*_) t),join (list h),_f,filter (less h _*_) t*h*t) (head _) (tail _)!l)*_!r)", "CList [CNum 3,CNum 4,CNum 4,CNum 5,CNum 8,CNum 9]")
