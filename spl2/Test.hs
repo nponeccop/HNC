ebb2691b3e0c57941bfe3b92ad58ce5cbc8b2547
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-10-28 22:28:23 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@190 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/Test.hs
+++ spl2/Test.hs
@@ -112,7 +112,7 @@ tests = [
 	,("(r!_*iff (join1 (pair 0b (l!_)),elist) (l!join1 (head _),elist))", "CList [CNum 3,CNum 4,CNum 4,CNum 5,CNum 8,CNum 9]", "TD \"list\" [T \"num\"]")
 	,("(r!_*iff (join1 (pair 0b (l!_)),elist) (l!(h*t*join1 h,_f,filter (z*less z h) t) (head _) (tail _)))", "CList [CNum 3,CNum 4,CNum 4,CNum 5,CNum 8,CNum 9]", "TD \"list\" [T \"num\"]")
 	,("(z*(z*sum (head z) 1) (join1 1,elist))", "CL (CL (CL (CL (CVal \"sum\") (K [CVal \"z\",CVal \"x\"])) (S [\"x\"])) (K [CNum 1])) (S [\"z\"])", "")
-	,("(r!h*f*join1 h,f,filter (z*less z h),elist)", "", "")
+	,("(h*f*join1 h,f,filter (z*less z h),elist)", "", "")
 	,("(r!_*iff (join1 (pair (0b) (l!elist)),elist) (l!(h*t*join1 h,_f,filter (z*less z h) t) (head _) (tail _)))", "", "")
 	,("(r!_*iff (join1 (pair (less (length _) 1) (l!_)),elist) (l!(h*t*concat (_f,filter (_*less _ h) t),join1 h,_f,filter (_*not,less _ h) t) (head _) (tail _)))", "CList [CNum 3,CNum 4,CNum 4,CNum 5,CNum 8,CNum 9]", "TD \"list\" [T \"num\"]")
 	,("(_*_,join1 8,join1 9,join1 4,join1 4,join1 5,join1 3,elist) (r!_*iff (join1 (pair (less (length _) 1) (l!_)),elist) (l!(h*t*concat (_f,filter (_*less _ h) t),join1 h,_f,filter (_*not,less _ h) t) (head _) (tail _)))", "CList [CNum 3,CNum 4,CNum 4,CNum 5,CNum 8,CNum 9]", "TD \"list\" [T \"num\"]")
