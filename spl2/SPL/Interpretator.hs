11b93c2d8debd15025abe37bbd5331e2564fafe7
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-11-29 23:05:02 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@311 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL/Interpretator.hs
+++ spl2/SPL/Interpretator.hs
@@ -14,31 +14,28 @@ data P = P ([Char], [Char]) | N ([Char], [Char])
 step str =
 	case parse str of
 		SPL.Parser.P _ i p ->
-			case compile p of
-				c ->
-					case check0 c of
-						SPL.Check3.P (ur, a)|M.null ur -> SPL.Interpretator.P (show a, show $ eval0 $ c_of_cp c)
-						SPL.Check3.P (u2, a) -> error ("check0 returned saved vars: "++show u2++"\n"++show str)
-						SPL.Check3.N e -> SPL.Interpretator.N $ ("type error: " ++ e, show c)
+			let c = compile p in
+				case check0 c of
+					SPL.Check3.P (ur, a)|M.null ur -> SPL.Interpretator.P (show a, show $ eval0 $ c_of_cp c)
+					SPL.Check3.P (u2, a) -> error ("check0 returned saved vars: "++show u2++"\n"++show str)
+					SPL.Check3.N e -> SPL.Interpretator.N $ ("type error: " ++ e, show c)
 		SPL.Parser.N i ->
 			SPL.Interpretator.N ("  "++(take i $ repeat ' ')++"^ parser error", "")
 
 get_code_of_expr str =
 	case parse str of
 		SPL.Parser.P _ i p ->
-			case compile p of
-				c -> SPL.Interpretator.P (show c, "")
+			SPL.Interpretator.P (show $ compile p, "")
 		SPL.Parser.N i ->
 			SPL.Interpretator.N ("  "++(take i $ repeat ' ')++"^ parser error", "")
 
 get_type_debug_of_expr str =
 	case parse str of
 		SPL.Parser.P _ i p ->
-			case compile p of
-				c ->
-					case check0 c of
-						SPL.Check3.P (ur, a) -> SPL.Interpretator.P (show $ (ur, a), "")
-						SPL.Check3.N e -> SPL.Interpretator.N $ ("type error: " ++ e, show c)
+			let c = compile p in
+				case check0 c of
+					SPL.Check3.P (ur, a) -> SPL.Interpretator.P (show $ (ur, a), "")
+					SPL.Check3.N e -> SPL.Interpretator.N $ ("type error: " ++ e, show c)
 		SPL.Parser.N i ->
 			SPL.Interpretator.N ("  "++(take i $ repeat ' ')++"^ parser error", "")
 
