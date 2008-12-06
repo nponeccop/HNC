bc5644720c8d6ecae3346b669837705973a38197
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-06 23:44:32 +0000

    New: --spl debug option for HNC
    git-svn-id: http://inv.googlecode.com/svn/trunk@367 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HNC.hs
+++ spl2/HNC.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.Map as M
 
 import HN.Parser2
+import HN.SplExport
 import CPP.Core
 import Utils
 import CPP.Intermediate
@@ -16,10 +17,14 @@ tdi2 = DefinitionInherited {
 ,	diType         = Nothing
 }
 
-compileFile inFile = parseFile inFile >>= return . (++) "#include <hn/lib.hpp>\n\n" . show . dsCppDef . sem_Definition tdi2 . head . fromRight
+compile inFile f = parseFile inFile >>= return . f . head . fromRight
+
+compileFile inFile = compile inFile $ (++) "#include <hn/lib.hpp>\n\n" . show . dsCppDef . sem_Definition tdi2
+
+compileToSpl inFile = compile inFile convertDef
 
 main = getArgs >>= f where
+	f [inFile, "--spl"] = compileToSpl inFile >>= print
 	f [inFile, outFile] = compileFile inFile >>= writeFile outFile
 	f [inFile] = compileFile inFile >>= putStr
-	f _ = putStrLn "Usage: hnc <infile> [<outfile>]\n"
- 
+	f _ = putStrLn "Usage: hnc <infile> [<outfile> | --spl]\n"
