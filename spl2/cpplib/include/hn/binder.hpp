772f6959604442ccb30b53f43a95934eabeb23fd
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-06 21:29:08 +0000

    New: initial implementation of hn::bind by zabivator
    git-svn-id: http://inv.googlecode.com/svn/trunk@365 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/cpplib/include/hn/binder.hpp
+++ spl2/cpplib/include/hn/binder.hpp
@@ -1,26 +1,44 @@
 #pragma once
 
-namespace hn {
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 
-template <typename IMPL>
-struct binder
+namespace hn
 {
-	IMPL * _this;
-
-	void (IMPL::*method)();
-
-	void operator()()
+	template< typename ReturnType, typename Target >
+	boost::function< ReturnType() > bind( Target a_target, ReturnType (Target::*a_method)() )
 	{
-		(_this->*method)();
+		return boost::bind( a_method, a_target );
 	}
-};
 
+	template< typename ReturnType, typename Target, typename T0 >
+	boost::function< ReturnType( T0 ) > bind( Target a_target, ReturnType (Target::*a_method)( T0 ) )
+	{
+		return boost::bind( a_method, a_target, _1 );
+	}
 
-template <typename IMPL>
-binder<IMPL> bind(IMPL &_this, void (IMPL::*method)() )
-{
-	binder<IMPL> b = { &_this, method };
-	return b;
-}
+	template< typename ReturnType, typename Target, typename T0, typename T1 >
+	boost::function< ReturnType( T0, T1 ) > bind( Target a_target, ReturnType (Target::*a_method)( T0, T1 ) )
+	{
+		return boost::bind( a_method, a_target, _1, _2 );
+	}
 
-};
+	template< typename ReturnType, typename Target, typename T0, typename T1, typename T2 >
+	boost::function< ReturnType( T0, T1, T2 ) > bind( Target a_target, ReturnType (Target::*a_method)( T0, T1, T2 ) )
+	{
+		return boost::bind( a_method, a_target, _1, _2, _3 );
+	}
+
+	template< typename ReturnType, typename Target, typename T0, typename T1, typename T2, typename T3 >
+	boost::function< ReturnType( T0, T1, T2, T3 ) > bind( Target a_target, ReturnType (Target::*a_method)( T0, T1, T2, T3 ) )
+	{
+		return boost::bind( a_method, a_target, _1, _2, _3, _4 );
+	}
+
+	template< typename ReturnType, typename Target, typename T0, typename T1, typename T2, typename T3, typename T4 >
+	boost::function< ReturnType( T0, T1, T2, T3, T4 ) > bind( Target a_target, ReturnType (Target::*a_method)( T0, T1, T2, T3, T4 ) )
+	{
+		return boost::bind( a_method, a_target, _1, _2, _3, _4, _5 );
+	}
+} // namespace hn
+
