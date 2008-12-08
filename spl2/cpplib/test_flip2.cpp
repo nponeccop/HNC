2e0e0dbcfc31bccbfaf970e762788974a7eb0770
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-08 09:30:44 +0000

    New: enhancements for IO and C++ compilation test suite
    git-svn-id: http://inv.googlecode.com/svn/trunk@374 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/cpplib/test_flip2.cpp
+++ spl2/cpplib/test_flip2.cpp
@@ -19,6 +19,13 @@ boost::function<c (b, a)> flip(boost::function<c (a, b)> f)
 	return hn::bind(impl, &local::flipped);
 };
 
+template <typename a, typename b, typename c>
+int flip2(boost::function<c (a, b)> f)
+{
+	typedef flip_impl<a, b, c> local;
+	local impl = { f };
+	return 5;
+};
 
 int sub(int a, int b)
 {
@@ -28,5 +35,6 @@ int sub(int a, int b)
 void main()
 {
 	boost::function<int (int, int)> _sub = sub;
-	printf("%d\n", (flip(_sub))(3, 2));
+	ff::print(flip(_sub)(3, 2));
+	ff::print(flip2(_sub));
 }
