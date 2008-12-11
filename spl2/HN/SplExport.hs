76c8d9471d593a26529d1963b3231cbcb0fccd4f
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-11 14:41:04 +0000

    Fix: S [] was generated by HN.SplExport.convertDefinition
    git-svn-id: http://inv.googlecode.com/svn/trunk@392 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HN/SplExport.hs
+++ spl2/HN/SplExport.hs
@@ -10,13 +10,15 @@ convertExpr (Application a b) = CL (convertExpr a) (K $ map convertExpr b)
 convertExpr expr = error $ show expr 
 
 convertDef (Definition _ [] value []) = convertExpr value
--- convertDef def @ (Definition _ [] value _) = error $ show def
 
 convertDef (Definition _ arguments value whereDefinitions) 
-	= CL xvalue (S arguments) where
+	= (case arguments of
+		[] -> convertedWithWhere
+		_ -> CL xvalue (S arguments)) where
 		xvalue = case whereDefinitions of
 			[] -> convertExpr value
-			_  -> CL (CL (convertExpr value) (S whereVars)) (K whereValues)
+			_  -> convertedWithWhere
 		whereVars = whereMap (\(Definition name _ _ _) -> name)
 		whereValues = whereMap convertDef
 		whereMap f = map f whereDefinitions
+		convertedWithWhere = CL (CL (convertExpr value) (S whereVars)) (K whereValues)
