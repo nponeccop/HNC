90a2aeeea153134c0152afc83ba72e4419ff9be6
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-15 22:41:50 +0000

    Fix: Hugs.Observe was not commented
    git-svn-id: http://inv.googlecode.com/svn/trunk@410 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL/Check3.hs
+++ spl2/SPL/Check3.hs
@@ -6,7 +6,7 @@ import Data.Map as M hiding (filter, union)
 import SPL.Types
 import SPL.Top
 --import Debug.Trace
-import Hugs.Observe
+--import Hugs.Observe
 --observe a b = b
 --trace2 a b = trace ("<\n"++a++"\n  "++show b++"\n>") b
 observeN a b = b
