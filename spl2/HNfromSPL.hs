def5c6bb890df1e602b3f523840cb79bc3854f4a
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-30 21:40:21 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@468 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HNfromSPL.hs
+++ spl2/HNfromSPL.hs
@@ -20,15 +20,32 @@ tdi rt = DefinitionInherited {
 ,       diRootTypes    = rt
 }
 
-compile = (dsCppDef . z) where
-	z self @ (Definition name _ _ _) = sem_Definition (tdi types) self where
+compile =
+	(dsCppDef . z)
+		where
+		z self @ (Definition name _ _ _) = sem_Definition (tdi types) self
+			where
 			P (fv, x) = check1 (convertDef self) SPL.Top.get_types
 			types = M.insert name x fv
 
 
 comp (CNum n) =
-	Definition "num" [] (Constant $ ConstInt n) []
+	Constant $ ConstInt n
+comp (CVal v) =
+	Atom v
+comp (CL c (K p)) =
+	Application (comp c) (Prelude.map comp p)
+comp (CL c (S p)) =
+	Lambda p (comp c)
+--comp (CL c (W ((n,v):ws))) =
+--	Application (comp c) (Prelude.map comp p)
+comp o =
+	-- Constant $ ConstInt 0
+	error $ show o
 
-res = compile $ comp $ (CNum 1)
+c = CL (CL (CL (CVal "foldr") (K [CVal "g",CVal "elist",CVal "l"])) (W [("g",CL (CL (CVal "join1") (K [CL (CVal "f") (K [CVal "x"]),CVal "y"])) (S ["x","y"]))])) (S ["f","l"])
+
+res = compile $ Definition "main" [] (comp c) []
+res2 = Definition "main" [] (comp c) []
 
 
