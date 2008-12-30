b88043097758b592feabf6cc8b32f5d168990374
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2008-12-30 13:14:59 +0000

    test.spl fixed
    git-svn-id: http://inv.googlecode.com/svn/trunk@463 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL/Check3.hs
+++ spl2/SPL/Check3.hs
@@ -219,6 +219,7 @@ check (CL a (D n)) et sv =
 				Just a -> P (M.empty, a)
 				Nothing -> error ("struct100:"++show m)
 		P (_, TV n2) -> P (M.singleton n2 (TS $ M.singleton n (TU (n2++"."++n))), TU n)
+		P (_, TU n2) -> P (M.empty, TU n)
 		N i o -> N i o
 
 check (CL a L) et sv =
