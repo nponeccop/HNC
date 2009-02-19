469b19cd9a3ce30df67569fc5e8add51cd8ffc88
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2009-02-19 16:13:55 +0000

    New: Better size control for Arbitrary Types.C
    git-svn-id: http://inv.googlecode.com/svn/trunk@520 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/QuickCheck.hs
+++ spl2/QuickCheck.hs
@@ -9,10 +9,10 @@ instance Arbitrary SPL.Types.C where
 	arbitrary = sized $ \sz -> oneof [arb_cnum [] sz, arb_cbool [] sz, arb_sum sz] where
 		-- произвольные выражения типа CNum
 		arb_cnum l 0 = oneof $ (return $ CNum 2) : l
-		arb_cnum l sz = oneof $ (return $ CNum 2) : arb_sum (sz - 1) : arb_fooInt (sz - 1) : l
+		arb_cnum _ sz = oneof $ [arb_sum (sz - 1), arb_fooInt (sz - 1)]
 
 		arb_cbool l 0 = oneof $ (return $ CBool True) : l
-		arb_cbool l sz = oneof $ (return $ CBool True) : (arb_less $ sz - 1) : arb_fooBool (sz - 1) : l
+		arb_cbool _ sz = oneof $ [arb_less (sz - 1), arb_fooBool (sz - 1)]
 
  		arb_sum sz = arb_binaryFunc sz f (arb_cnum []) (arb_cnum []) where
 			f = \x y -> CL (CVal "sum") $ K [x, y]
@@ -38,7 +38,7 @@ instance Arbitrary SPL.Types.C where
 			a2 <- arg2 (sz - arg1sz)
 			return $ f a1 a2
 			
-prop_Foo xs = (length $ show xs) < 200 ==> case SPL.Top.check2 xs of
+prop_Foo xs = (length $ show xs) < 500 ==> case SPL.Top.check2 xs of
 	P _ -> True
 	_ -> False
 
