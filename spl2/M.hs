3a19bdb6719d259c1a60a7a722fa34a0f415f208
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2009-06-08 20:27:20 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@551 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/M.hs
+++ spl2/M.hs
@@ -9,8 +9,8 @@ import SPL.Top
 --_in = (CL (CL (CNum 1) (W [("foo",CNum 2)])) (W [("foo",CBool True)]))
 -- _in = CL (CL (CVal "sum") (K [CVal "a"])) (S ["a"])
 
---s = "(x*sum x,(y*sum y 1) 3)"
-s = "(x*y*sum x y) 5 6"
+s = "(x*sum x,(y*sum y 1) 3)"
+--s = "(x*y*sum x y) 5 6"
 
 res = a where SPL.Interpretator.P (a, _) = get_type_tree_of_expr s
 res1 = show $ get_code_of_expr s
