f801b03ea80b65d82374a0343c3ee2d28bd990d3
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2009-09-24 22:15:02 +0000

    str.reverse fixed
    git-svn-id: http://inv.googlecode.com/svn/trunk@604 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL/Lib/Str.hs
+++ spl2/SPL/Lib/Str.hs
@@ -6,14 +6,18 @@ lib =
 	("str_concat", do_concat)
 	:("str_reverse", do_reverse)
 	:("str_eq", do_eq)
+	:("str_num", do_num)
 	:[]
 
 do_concat (CStr s1:CStr s2:[]) e =
 	CStr (s1++s2)
 
 do_reverse (CStr s:[]) e =
-	CStr (reverse $ show s)
+	CStr (reverse s)
 
 do_eq (CStr s1:CStr s2:[]) e =
 	CBool (s1 == s2)
+
+do_num (CNum n:[]) e =
+	CStr $ show n
 
