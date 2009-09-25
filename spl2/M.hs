6b60ea78342c37d83192c2ad07233e985b0ff1e8
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2009-09-25 08:29:36 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@608 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/M.hs
+++ spl2/M.hs
@@ -1,11 +1,29 @@
 module Main where
 
+import SPL.Compiler
+import SPL.Top
+import SPL.Check3
+import SPL.Interpretator
 import SPL.Parser2
+import Data.Map as M
 
 --s = "{f a b *a:sum 1 2}"
-s = "1"
+s = "sum 1"
 
-res = parse s
+recompile c r =
+	show r
+
+res =
+	case parse s of
+		SPL.Parser2.P _ i p _ ->
+			let c = compile p in
+				case check2 c of
+					SPL.Check3.P (r, ur, a)|M.null ur -> recompile c r
+					SPL.Check3.P (_, u2, a) -> error ("check0 returned saved vars: "++show u2++"\n"++show s)
+					SPL.Check3.N i e -> "type error: "
+		SPL.Parser2.N i _ ->
+			"parser error"
+
 
 main = putStrLn $ s++"\n\n"++(show Main.res)
 
