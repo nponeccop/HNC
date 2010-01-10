19cd7563574cc6318bdf69cdd74ab9490468384d
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-01-10 23:27:01 +0000

    New: better tracing messages in HNC --types mode
    git-svn-id: http://inv.googlecode.com/svn/trunk@682 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HNC.hs
+++ spl2/HNC.hs
@@ -13,6 +13,7 @@ import SPL.Types
 import SPL.Check3
 import SPL.Visualise
 import System.Environment
+import CPP.TypeProducer
 
 tdi2 t types typed = DefinitionInherited {
 	diLevel        = 0
@@ -36,6 +37,27 @@ compileFile t inFile
 typeCheck inFile = compile inFile f where
 	f self = check1 (convertDef self) SPL.Top.get_types
 
+print2 (P (x, _, _)) = do
+	putStrLn $ printFF False x
+	putStrLn ""
+	putStrLn $ printFF True x
+	putStrLn ""
+	print x
+
+
+makeType (T x) = x
+makeType (TD a b) =  a ++ "<" ++ concatMap makeType b ++ ">"
+makeType (TT x) = joinStr " -> " $ map makeType x
+makeType (TU x) = "?" ++ x
+makeType x = show x
+
+printFF a (CTyped x y) = (if a then "[" ++ makeType x ++ "] " else "" ) ++  printFF a y
+printFF a (CL x (K y)) = "(" ++ (printFF a x) ++ " " ++ (concatMap (printFF a) y) ++ ")"
+printFF a (CL x (S y)) =  "(\\" ++ (concatMap (\x -> x ++ " ") y) ++ "-> " ++ (printFF a x) ++ ")"
+printFF _ (CVal x) = x
+printFF _ (CNum x) = show x
+printFF _ x = show x
+
 compileToSpl inFile = do
 	x <- compile inFile convertDef
 	return $ show x ++ "\n" ++ showAsSource x
@@ -43,7 +65,7 @@ compileToSpl inFile = do
 main = getArgs >>= f where
 	f [inFile, "--spl"] = compileToSpl inFile >>= putStr
 	f [inFile, "--trace-p"] = compileFile True inFile >>= putStr
-	f [inFile, "--types"] = typeCheck inFile >>= print
+	f [inFile, "--types"] = typeCheck inFile >>= print2
 	f [inFile, outFile] = compileFile False inFile >>= writeFile outFile
 	f [inFile] = compileFile False inFile >>= putStr
 	f _ = putStrLn "Usage: hnc <infile> [<outfile> | --spl | --trace-p | --types ]\n"
