2cea109119873e2ead07a3bed41da3bda8026531
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-01-25 16:53:14 +0000

    tmp
    git-svn-id: http://inv.googlecode.com/svn/trunk@715 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL/Optimizer1.hs
+++ spl2/SPL/Optimizer1.hs
@@ -1,22 +1,27 @@
 module Optimizer1 where
 
 import SPL.Types
+import SPL.Check3 hiding (res)
 
 _in = CTyped (T "num") (CL (CL (CTyped (TU "a") (CL (CL (CVal "a") (S ["y"])) (K [CTyped (TU "a") (CVal "a")]))) (S ["a"])) (K [CTyped (T "num") (CNum 5)]))
 
 opt (CTyped t c) =
 	CTyped t $ opt c
-opt (CL (CL c (S (s:[]))) (K (k:[]))) =
-	CL (CL (opt_val c s) (S (s:[]))) (K (k:[]))
+opt (CL (CL c (S (s:[]))) (K ((CTyped t k):[]))) =
+	CL (CL (opt_val c s t) (S (s:[]))) (K ((CTyped t k):[]))
 opt o =
 	error ("Optimizer: "++show o)
 
-opt_val (CTyped t c) s =
-	CTyped t (opt_val c s)
-opt_val c s = c
-	--error $ show c
+opt_val (CTyped t2 (CL c (K [CTyped (TU v1) (CVal v2)]))) s t =
+	CTyped (opt_type t2 v1 t) $ CL c (K [CTyped t (CVal v2)])
+
+opt_type (TU a) v1 t|a == v1 =
+	t
 
 res = opt _in
 
-main = putStrLn $ show res
+main = putStrLn $ (show _in)++"\n"++(show res)
+
+
+
 
