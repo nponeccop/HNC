dd12103298a74baf6aa0bb1f09255b0918ad69c0
inv2004 <inv2004@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-03-03 00:48:13 +0000

    invalid test commited
    git-svn-id: http://inv.googlecode.com/svn/trunk@724 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/SPL_Tests.hs
+++ spl2/SPL_Tests.hs
@@ -143,6 +143,7 @@ tests = [
 	,("a/2~(if_a (a*b*b) {0} a)*a:mk_a 1b 7", "CNum 7", "T \"num\"")
 	,("ab/4~mk_ab 1 1b 'true'", "CL (CInFun 5 InFun \"typ_TT [T \"string\", TU \"ab4_1\", TU \"ab4_2\", TU \"ab4_3\", TU \"ab4_4\", T \"ab4\"]\") (K [CStr \"ab\",CNum 1,CBool True,CStr \"true\"])", "TT [TU \"a\",T \"ab4\"]")
 	,("[a/1~]", "", "")
+	,("a/1~if_a (a*a) {0},b.mk_a 7*b:[a/1~]", "", "") -- error
 --	,("f 2*a:200*f:((b*sum a b)*a:100)", "CNum 102", "T \"num\"")
 --	,("f 10*f:('a*b*if (less b a) {1b} {_f incr/a b})/2", "", "")
 --	,("{incr:(x*{b:sum x})}.incr 1,.b", "", "")
