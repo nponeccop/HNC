2b868f9644eb8254a5033f847cd2c3017040a9da
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-05-04 20:25:09 +0000

    Internal: Attributes are refactored and reordered in AG
    git-svn-id: http://inv.googlecode.com/svn/trunk@773 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/CPP/Core.hs
+++ spl2/CPP/Core.hs
@@ -35,25 +35,6 @@ sem_Definition inh self @ (Definition name args val wh)
 		dsCppDef = AG.cppDefinition_Syn_Definition $ AG.wrap_Definition (AG.sem_Definition self) AG.Inh_Definition {
 				AG.level_Inh_Definition = diLevel inh
 			, 	AG.typed_Inh_Definition = diTyped inh
-
-			-- в выражениях С++ обращение к символу foo выполняется многими способами:
-			-- local::foo - если foo статическая функция в bar_impl
-			-- impl.foo - если foo переменная в bar_impl или (нестатическая функция, и выполняется её вызов)
-			-- hn::bind(impl, &local::foo) - если foo нестатическая функция и выполняется передача её параметром
-			-- &local::foo - если foo статическая функция и выполняется передача её параметром
-			-- foo - если foo локальная переменная функции или параметр функции
-			-- hn::foo - если foo является функцией стандартной библиотеки
-			--
-			-- Способы ниже не реализованы:
-			-- при каррировании необходимо вызывать hn::curry1(&local::foo) или hn::curry1(impl, &local::foo)
-			-- при вложеннности замыканий необходимо вместо local указывать цепочку parent::parent::foo или up.up.foo
-			--
-			-- Для поддержки этих разных обращений сделан слой symTabTranslator, fqn, fqnTransformer
-			-- fqnTransformer показывает, выполняется ли обращение к функции или передача её параметром
-			-- т.е. один и тот же атом транслируется по-разному, в зависимости от того, находится
-			-- он в позиции функции или в позиции аргумента
-			-- изначально из компилятора передается таблица библиотечных символов
-			-- на основе S
 			,   AG.symTab_Inh_Definition = diSymTab inh
 			,   AG.inferredType_Inh_Definition = diInferredType inh
 			}
