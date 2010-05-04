a2bb9f844b93e7173a39279d4d0f8d9e6e8f8a44
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-05-04 18:45:34 +0000

    Internal: All compilation is done in AG now
    git-svn-id: http://inv.googlecode.com/svn/trunk@772 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/CPP/Core.hs
+++ spl2/CPP/Core.hs
@@ -32,12 +32,7 @@ data DefinitionSynthesized = DefinitionSynthesized {
 
 sem_Definition inh self @ (Definition name args val wh)
 	= DefinitionSynthesized {
-		dsCppDef = (AG.cppDefinition_Syn_Definition semDef) {
-			functionContext			=  fmap (\ctt -> ctt { contextMethods = semWhere }) $ functionContext $ AG.cppDefinition_Syn_Definition semDef
-		}
-	} where
-
-		agInh = AG.Inh_Definition {
+		dsCppDef = AG.cppDefinition_Syn_Definition $ AG.wrap_Definition (AG.sem_Definition self) AG.Inh_Definition {
 				AG.level_Inh_Definition = diLevel inh
 			, 	AG.typed_Inh_Definition = diTyped inh
 
@@ -62,19 +57,4 @@ sem_Definition inh self @ (Definition name args val wh)
 			,   AG.symTab_Inh_Definition = diSymTab inh
 			,   AG.inferredType_Inh_Definition = diInferredType inh
 			}
-		semDef = AG.wrap_Definition (AG.sem_Definition self) agInh
-
-		semWhere = map (\x -> x { functionTemplateArgs = [] }) $ sem_WhereMethods inh1 (diTyped inh1) wh where
-			inh1 = inh { diLevel = AG.level_Inh_Definition agInh + 1 }
-
-sem_WhereMethods inh whereTyped wh = map mf typedMethods where
-	typedDef = zip wh (AG.unfoldTyped $ diTyped inh)
-	typedMethods = filter (isFunction . fst) typedDef
-	mf (method, (t, ww)) = dsCppDef $ sem_Definition newInh method where
-		newInh = inh { diInferredType = t, diTyped = ww }
-
-isFunction (Definition _ args _ _) = not $ null args
-
-
-
-defName (Definition name _ _ _) = name
+	}
