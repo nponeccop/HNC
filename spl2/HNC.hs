fdac7eae6d8354a73376c830f51dd102721eec6d
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-05-05 11:13:03 +0000

    Internal: Advanced visualizations of SPL.C and SPL.T used by hnc --types are moved to SPL.Visualise
    git-svn-id: http://inv.googlecode.com/svn/trunk@777 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HNC.hs
+++ spl2/HNC.hs
@@ -6,11 +6,10 @@ import System.Environment
 import Utils
 
 import CPP.CompileTools
+
 import HN.Parser2
 import HN.SplExport
-import HN.Intermediate
 
-import SPL.Types
 import SPL.Check3
 import SPL.Visualise
 
@@ -22,33 +21,12 @@ compileFile inFile
 
 typeCheck inFile = compile inFile typecheckDefinition
 
-
-print2 (P (x, _, _)) = do
-	putStrLn $ printFF False x
+dumpInferredTypes (P (x, _, _)) = do
+	putStrLn $ showTypedTreeWithoutTypes x
 	putStrLn ""
-	putStrLn $ printFF True x
+	putStrLn $ showTypedTree x
 	putStrLn ""
 	print x
-
-makeType (T x) = x
-makeType (TD a b) =  a ++ " " ++ (joinStr " " $ map makeType b)
-makeType (TT x) = joinStr " -> " $ map makeType2 x
-makeType (TU x) = '?' : x
-makeType x = show x
-
-makeType2 (x @ (TT _)) = "(" ++ makeType x ++ ")"
-makeType2 x = makeType x
-
-printFF a (CTyped x y) = (if a then "[" ++ makeType x ++ "] " else "" ) ++  printFF a y
-printFF a (CL x (K y)) = "(" ++ printFF a x ++ " " ++ (joinStr " " $ map (printFF a) y) ++ ")"
-printFF a (CL x (S y)) =  "(\\" ++ concatMap (++ " ") y ++ "-> " ++ printFF2 a x ++ ")"
-printFF _ (CVal x) = x
-printFF _ (CNum x) = show x
-printFF _ x = show x
-
-printFF2 a (CL x (K y)) = printFF a x ++ " " ++ (joinStr " " $ map (printFF a) y)
-printFF2 a (CTyped x y) = if a then "[" ++ makeType x ++ "] " ++ printFF a y else printFF2 a y
-printFF2 a b = printFF a b
 
 compileToSpl inFile = do
 	x <- compile inFile convertDef
@@ -56,7 +34,7 @@ compileToSpl inFile = do
 
 main = getArgs >>= f where
 	f [inFile, "--spl"] = compileToSpl inFile >>= putStr
-	f [inFile, "--types"] = typeCheck inFile >>= print2
+	f [inFile, "--types"] = typeCheck inFile >>= dumpInferredTypes
 	f [inFile, outFile] = compileFile inFile >>= writeFile outFile
 	f [inFile] = compileFile inFile >>= putStr
-	f _ = putStrLn "Usage: hnc <infile> [<outfile> | --spl | --trace-p | --types ]\n"
+	f _ = putStrLn "Usage: hnc <infile> [<outfile> | --spl | --types ]\n"
--- spl2/SPL/Visualise.hs
+++ spl2/SPL/Visualise.hs
@@ -34,5 +34,28 @@ isApp _ = False
 isWhere (CL _ (W _)) = True
 isWhere _ = False
 
-isComposed (CL _ _) = True 
+isComposed (CL _ _) = True
 isComposed _  = False
+
+makeType (T x) = x
+makeType (TD a b) =  a ++ " " ++ (joinStr " " $ map makeType b)
+makeType (TT x) = joinStr " -> " $ map makeType2 x
+makeType (TU x) = '?' : x
+makeType x = show x
+
+makeType2 (x @ (TT _)) = "(" ++ makeType x ++ ")"
+makeType2 x = makeType x
+
+printFF a (CTyped x y) = (if a then "[" ++ makeType x ++ "] " else "" ) ++  printFF a y
+printFF a (CL x (K y)) = "(" ++ printFF a x ++ " " ++ (joinStr " " $ map (printFF a) y) ++ ")"
+printFF a (CL x (S y)) =  "(\\" ++ concatMap (++ " ") y ++ "-> " ++ printFF2 a x ++ ")"
+printFF _ (CVal x) = x
+printFF _ (CNum x) = show x
+printFF _ x = show x
+
+printFF2 a (CL x (K y)) = printFF a x ++ " " ++ (joinStr " " $ map (printFF a) y)
+printFF2 a (CTyped x y) = if a then "[" ++ makeType x ++ "] " ++ printFF a y else printFF2 a y
+printFF2 a b = printFF a b
+
+showTypedTree x = printFF True x
+showTypedTreeWithoutTypes x = printFF False x
