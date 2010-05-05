7b1bd2d4afcd84f8ea169009ff8f7fdcf002221a
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-05-05 10:56:21 +0000

    Internal: Minor refactorings in CPP.CompilerTools to eliminate legacy tdi2 function
    git-svn-id: http://inv.googlecode.com/svn/trunk@776 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/CPP/CompileTools.hs
+++ spl2/CPP/CompileTools.hs
@@ -8,14 +8,13 @@ import SPL.Top
 import SPL.Check3
 import qualified Bar as AG
 
-compileDefinition self  = tdi2 typed x self where
-	P (typed, _, x) = typecheckDefinition self
-
-	tdi2 typed inferredType self = AG.compile self AG.Inh_Definition {
+compileDefinition self  = AG.compile self inh where
+	P (typedCodeTree, _, rootDefinitionType) = typecheckDefinition self
+	inh = AG.Inh_Definition {
 			AG.level_Inh_Definition = 0
-		, 	AG.typed_Inh_Definition = typed
+		, 	AG.typed_Inh_Definition = typedCodeTree
 		,   AG.symTab_Inh_Definition = M.map (const $ CppFqMethod "ff") SPL.Top.get_types
-		,   AG.inferredType_Inh_Definition = inferredType
+		,   AG.inferredType_Inh_Definition = rootDefinitionType
 		}
 
 typecheckDefinition self = check1 (convertDef self) SPL.Top.get_types
