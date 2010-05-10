6b0d2f32b9cd31b29bed49970a4078144ec508ee
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-05-10 15:20:24 +0000

    Internal: Some tests were enabled back in HN_Tests.hs as they work now
    git-svn-id: http://inv.googlecode.com/svn/trunk@806 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HN_Tests.hs
+++ spl2/HN_Tests.hs
@@ -41,37 +41,37 @@ testSet =
 	,	"main sum = incr sum"
 		-- перекрытие fqn-функции статической локальной
 		-- BROKEN не должно быть typedef local
---	,	"main x = { elist a b = sum a b\nelist x x }"
+	,	"main x = { elist a b = sum a b\nelist x x }"
 		-- перекрытие fqn-функции локальной переменной
---	,	"main x z = { head = incr z\nsum x head }"
+	,	"main x z = { head = incr z\nsum x head }"
 		-- константные строки
 	,	"main = \"aaa\""
 		-- локальное замыкание c аргументом
---	,	"main a b = { c i = sum i b\nincr (sum a (c a)) }"
+	,	"main a b = { c i = sum i b\nincr (sum a (c a)) }"
 
 --	,	"main a b = { c i x = sum b (sum x i)\nd i = sum i i\nincr (c a (d a)) }"
 		-- локальная переменная
---	,	"main x = { y = sum x x\nsum y y }"
+	,	"main x = { y = sum x x\nsum y y }"
 		-- & перед указателями на функции (рекомендуется новым стандартом C++)
 		-- биндеры при передаче локальных функций аргументами
 		-- f x y a -> f(x, y, hn::bind(impl, &main_impl::a))
 --	,	"main x z l = { a = incr z\ny z = less (sum x a) z\nfilter y l }"
 		-- BROKEN & перед указателями на статические функции
---	,	"main l = {\n\tf x = less 1 x\n\tfilter f l\n}"
+	,	"main l = {\n\tf x = less 1 x\n\tfilter f l\n}"
 
 	-- l*((f*(filter f l)) (x*less 1 x))
 		-- перекрытие fqn-функции статической
---	,	"main x = { head z a = z a x\nhead sum 5 }"
+	,	"main x = { head z a = z a x\nhead sum 5 }"
 			-- статическая функция
 		-- BROKEN не должно быть typedef local
---	,	"main x = { o a b = sum a b\no x x }"
+	,	"main x = { o a b = sum a b\no x x }"
 		-- polymorphic function
 	,	"main l f = filter f l"
 	-- polymorphic function without free variables
---	,	"flip f = { flipped x y = f y x\nflipped }"
---	,	"map f l = {\n\tg x y = join1 (f x) y\n\tfoldr g elist l }"
+	,	"flip f = { flipped x y = f y x\nflipped }"
+	,	"map f l = {\n\tg x y = join1 (f x) y\n\tfoldr g elist l }"
 		-- parameter name conflicts with inferred type variable name
---	,	"main a f = filter f a"
+	,	"main a f = filter f a"
 	]
 
 defaultEnv = Env 1 $ M.fromList $ map (\(a, b) -> (a, simpleParse2 b)) [
