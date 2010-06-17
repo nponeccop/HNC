141cd3037a21380cc617b39e82a3802954d098f6
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-06-17 15:05:46 +0000

    Fix: A hacked cpp source was submitted by mistake
    git-svn-id: http://inv.googlecode.com/svn/trunk@832 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/hn_tests/udp_echo_server2.cpp
+++ spl2/hn_tests/udp_echo_server2.cpp
@@ -8,20 +8,11 @@ struct hnMain_impl
 	{
 		return ff::udp_reply(s, msg);
 	};
-
-	hnMain_impl() : s(ff::udp_listen(99))
-	{
-
-	}
-private:
-	hnMain_impl(hnMain_impl &x) : s(x.s)
-	{
-	};
 };
 
 ff::IO<void> hnMain()
 {
 	typedef hnMain_impl local;
-	local impl;
+	local impl = { ff::udp_listen(99) };
 	return ff::forever(ff::bind(ff::udp_receive(impl.s), hn::bind(impl, &local::reply)));
 };
