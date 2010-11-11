1df1c20462d67ec371d2ecdd70724de2024dbfed
andy.melnikov <andy.melnikov@6d06213f-d81a-0410-b1e1-093907fbe7c0>
2010-11-11 11:52:54 +0000

    New: pretty printing of HN.Definition into *.hn source
    git-svn-id: http://inv.googlecode.com/svn/trunk@892 6d06213f-d81a-0410-b1e1-093907fbe7c0

--- spl2/HN/Optimizer/Frontend.hs
+++ spl2/HN/Optimizer/Frontend.hs
@@ -6,7 +6,7 @@ import HN.Optimizer.Inbound
 import HN.Optimizer.Inliner2
 import HN.Optimizer.WithGraph
 
-import HN.Optimizer.Visualise
+import HN.Visualise
 
 runFB = runF >=> runB
 
@@ -14,7 +14,7 @@ xxx = ["incr", "print", "sum", "filter", "bind", "readnum", "natrec", "_if", "eq
 
 transform tf = withGraph (fromTuple . runSimpleUniqueMonad . runWithFuel infiniteFuel . tf . toTuple) xxx  where
 	toTuple agraph = (agraph, undefined, undefined)
-	fromTuple (agraph, _, _) = error $ foo $ agraph
+	fromTuple (agraph, _, _) = agraph
 
 optimize :: Definition -> Definition
-optimize = transform $ runFB >=> runFB
+optimize = (error . showD) . (transform $ runFB >=> runFB)
---
+++ spl2/HN/Visualise.hs
@@ -0,0 +1,24 @@
+module HN.Visualise (showD) where
+
+import HN.Intermediate
+import Utils
+
+showD x = joinStr "\n" $ showDD x
+
+showDD (Definition name args letIn) = case letWhere letIn of
+	[] -> [ name ++ concatMap (' ' :) args ++ " = " ++ showE (letValue letIn) ]
+	w -> [ name ++ concatMap (' ' :) args ++ " = {"] ++ showW w ++ ["\t" ++ showE (letValue letIn), "}"]
+
+showW l = map ("\t" ++) $ concatMap showDD l
+
+showE e = case e of
+	Atom a -> a
+	Constant a -> show a
+	Application (Atom a) b -> a ++ concatMap (\x -> " " ++ showFunctionArg x) b
+
+
+showFunctionArg e = case e of
+	Atom a -> a
+	Constant a -> show a
+	_ -> inParens (showE e)
+
