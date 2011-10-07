e127f1a6be545aaf01560dc8f7dac620514c4308
Yuri Kashnikov <yuri.kashnikoff@gmail.com>
2011-10-07 21:03:49 +0200

    Fix: WithParsing was missing. Merged with Andy's patch from the main SVN      repo.

---
+++ Test/Optimizer/WithParsing.hs
@@ -0,0 +1,21 @@
+module Test.Optimizer.WithParsing (tests, main) where
+import Test.HUnit hiding (test)
+
+import HN.Parser2
+import HN.Optimizer.WithGraph
+
+realTest inFile = TestCase $ do
+        x <- parseAndProcessFile inFile id
+        x @=? withGraph id ["print", "sub", "mul", "natrec", "sum", "incr"] x
+
+tests = "WithParsing" ~:  map (\x -> realTest ("hn_tests/" ++ x ++ ".hn"))
+        [
+-- "locals14"
+         "where2"
+        , "where"
+        , "plusX"
+        --, "locals2"
+        --, "euler6"
+        ]
+
+main = runTestTT tests
