e1852346f999e4820a30c5a999dad7755296ee49
Andy Melnikov <andy.melnikov@gmail.com>
2011-12-14 19:54:59 +0200

    Fix: expected and actual results in failed compiler tests were swapped

--- Test/Compiler.hs
+++ Test/Compiler.hs
@@ -8,8 +8,8 @@ import CPP.CompileTools
 comp2 f g x y = f $ g x y
 
 test testName =	liftM2 (comp2 (testName ~:) (~=?))
-	(compile2 id $ "hn_tests/" ++ testName ++ ".hn")
 	(readFile $ "hn_tests/" ++ testName ++ ".cpp")
+	(compile2 id $ "hn_tests/" ++ testName ++ ".hn")
 
 
 iotests =
