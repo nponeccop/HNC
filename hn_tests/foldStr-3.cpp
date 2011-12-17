3b594a56e80f53508858a9419845cfb3a872b6f8
unknown <Admin@.(none)>
2011-12-17 21:15:49 +0200

    New: old-style @s attributes were completely removed

--- hn_tests/foldStr-3.cpp
+++ hn_tests/foldStr-3.cpp
@@ -1,10 +1,10 @@
 #include <hn/lib.hpp>
 
-template <typename t16, typename t24, typename t25>
+template <typename t18, typename t24, typename t25>
 struct foldStr_impl
 {
 	typedef foldStr_impl self;
-	boost::function<t24 (t16, t25)> f;
+	boost::function<t24 (t18, t25)> f;
 
 	static bool neq(int a, int b)
 	{
@@ -16,18 +16,18 @@ struct foldStr_impl
 		ff::ptr<int> s = ff::snd(loopState);
 		return neq(ff::deref(s), 0);
 	};
-	std::pair<t24, ff::ptr<t25>> loopTrans(std::pair<t16, ff::ptr<t25>> loopState)
+	std::pair<t24, ff::ptr<t25>> loopTrans(std::pair<t18, ff::ptr<t25>> loopState)
 	{
 		ff::ptr<t25> s = ff::snd(loopState);
-		t16 e = ff::fst(loopState);
+		t18 e = ff::fst(loopState);
 		return ff::pair(f(e, ff::deref(s)), ff::next(s));
 	};
 };
 
-template <typename t1, typename t16, typename t2, typename t24, typename t25>
-t2 foldStr(boost::function<t24 (t16, t25)> f, t1 e, t2 s)
+template <typename t1, typename t18, typename t2, typename t24, typename t25>
+t2 foldStr(boost::function<t24 (t18, t25)> f, t1 e, t2 s)
 {
-	typedef foldStr_impl<t16, t24, t25> local;
+	typedef foldStr_impl<t18, t24, t25> local;
 	local impl = { f };
 	return s;
 };
