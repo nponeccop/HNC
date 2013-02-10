1f7f2ca9c8ff3ce96d04003abb6341cf68ff0e9d
Andy Melnikov <andy.melnikov@gmail.com>
2013-02-10 19:35:47 +0200

    Fix for #40 spl-test-hunit-exe fails under Linux
    because of CRLF mismatch

--- Test/Compiler.hs
+++ Test/Compiler.hs
@@ -2,13 +2,20 @@ module Test.Compiler (iotests) where
 import Test.HUnit hiding (test)
 import System.Directory
 import Control.Monad
-
+import System.IO
 import CPP.CompileTools
+
+windowsLineMode = NewlineMode { inputNL  = CRLF, outputNL = CRLF }
+
+readFileCRLF name = do
+	h <- openFile name ReadMode
+ 	hSetNewlineMode h windowsLineMode
+	hGetContents h
 
 comp2 f g x y = f $ g x y
 
 test testName =	liftM2 (comp2 (testName ~:) (~=?))
-	(readFile $ "hn_tests/" ++ testName ++ ".cpp")
+	(readFileCRLF $ "hn_tests/" ++ testName ++ ".cpp")
 	(compile2 id $ "hn_tests/" ++ testName ++ ".hn")
 
 
