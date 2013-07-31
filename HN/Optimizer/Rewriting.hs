e78100c37a7c7cd9bc20374c852d2b5371a50396
Andy Melnikov <andy.melnikov@gmail.com>
2013-07-31 16:13:18 +0300

    Internal: a minor refactoring

--- HN/Optimizer/Rewriting.hs
+++ HN/Optimizer/Rewriting.hs
@@ -5,26 +5,26 @@ import Data.Functor.Fixedpoint
 import Data.Maybe
 import Control.Applicative
 import Compiler.Hoopl
-import HN.Intermediate (Const)
 import HN.Optimizer.Node
 import HN.Optimizer.Visualise ()
 import Utils
 
 type Rewrite a = a -> Maybe a
-
+{-
 composeR :: Rewrite a -> Rewrite a -> Rewrite a
 composeR a b x = if ch then Just result else Nothing where
 	result = dropR b $ dropR a x
 	ch = changed (a x) || changed (b x)
+-}
 
 type ListFact = WithTopAndBot DefinitionNode	
 
-changed Nothing = False
-changed _ = True
+-- changed Nothing = False
+-- changed _ = True
 	
 dropR :: Rewrite a -> a -> a
 dropR a x = fromMaybe x (a x)
-	
+{-	
 unitR :: Rewrite a
 unitR = const Nothing
 	
@@ -36,6 +36,7 @@ apply1 cons rewriter el = fmap cons $ rewriter el
 
 apply2 :: (h -> t -> l) -> Rewrite h -> Rewrite t -> h -> t -> Rewrite l
 apply2 cons rh rt h t = undefined
+-}
 
 rewriteApplication :: ExpressionFix -> [ExpressionFix] -> FactBase ListFact -> Maybe ExpressionFix
 
@@ -59,7 +60,7 @@ inlineApplication formalArgs actualArgs f
 	= Just . dropR (rewriteExpression $ flip mapUnion f $ mapFromList $ zip formalArgs $ map (PElem . LetNode []) actualArgs) 
 
 rewriteArgs  :: FactBase ListFact -> Rewrite [ExpressionFix]
-rewriteArgs f [] = Nothing 
+rewriteArgs _ [] = Nothing 
 rewriteArgs f (h : t) = lift2 (rewriteExpression f) (rewriteArgs f) (:) h t 
 
 lift2 :: Rewrite t -> Rewrite a1 -> (t -> a1 -> a) -> t -> a1 -> Maybe a 
@@ -76,12 +77,13 @@ rewriteExpression2 f = process $ \expr -> case expr of
 	Atom a -> do 
 		([], e) <- processAtom "rewriteExpression2" a $ xtrace ("factBase-atom {" ++ show a ++ "}") f
 		return e
-	Application aa @ (Fix (Atom _), a') bb -> let
-		b = map fst bb
+	Application aa @ (Fix (Atom _), _) bb -> let
+		-- b = map fst bb
 		b' = map (uncurry fromMaybe) bb
 		a = fst aa
+		bChanged = any (isJust . snd) bb
 		in case processAtom2 "rewriteApplication.Single" a f of
-			Nothing -> (Fix . Application a) <$> rewriteArgs f b
+			Nothing -> if bChanged then Just $ Fix $ Application a b' else Nothing
 			Just ([], expr) -> Just $ Fix $ Application expr b' 
 			Just (args, expr) -> inlineApplication args b' f expr
 	Application aa bb -> rewriteApplication (fst aa) (map fst bb) f
