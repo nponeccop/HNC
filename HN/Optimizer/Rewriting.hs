3ded62854aece4a8fc24ebaa588d89e07319b145
Andrii Melnykov <andy.melnikov@gmail.com>
2015-06-24 05:56:49 +0300

    Refactor: further simplification of rewriteExpression

--- HN/Optimizer/Rewriting.hs
+++ HN/Optimizer/Rewriting.hs
@@ -6,12 +6,9 @@ import HN.Intermediate
 import HN.Optimizer.Node
 import HN.Optimizer.Visualise ()
 import HN.Optimizer.ExpressionRewriter
-import Utils
 
 type ListFact = WithTopAndBot DefinitionNode
 
-isDoubleAtomApplication (Application (Application (Atom _) _) _)= True
-isDoubleAtomApplication _ = False
 {-
    Инлайнинг двойной аппликации
 
@@ -36,12 +33,10 @@ isDoubleAtomApplication _ = False
    На выходе hnMain = sum baz bar
 -}
 
-rewriteDoubleAtomApplication :: FactBase ListFact -> Rewrite ExpressionFix
-rewriteDoubleAtomApplication f (Application (Application (Atom a) b) c) = case processAtom "rewriteApplication.Double.1" a f of
-	Nothing -> Nothing
-	Just ([], _) -> error "rewriteApplication.double.var"
-	Just (outerParams, Atom aOuterBody) -> case processAtom "rewriteApplication.Double.2" aOuterBody f of
-		Just (innerParams, innerBody) -> Just $ dropR (inlineApplication2 old new) innerBody where
+rewriteDoubleAtomApplication b c f aa = case aa of
+	([], _) -> error "rewriteApplication.double.var"
+	(outerParams, Atom aOuterBody) -> case processAtom "rewriteApplication.Double.2" aOuterBody f of
+		Just (innerParams, innerBody) -> dropR (inlineApplication2 old new) innerBody where
 			old = outerParams ++ innerParams
 			new = b ++ c
 		_ -> error "rewriteApplication.double.fn.Nothing"
@@ -58,17 +53,17 @@ processAtom err a f = case lookupFact a f of
 	_ -> Nothing
 
 rewriteExpression2 :: FactBase ListFact -> Rewrite ExpressionFix
-rewriteExpression2 = process' . phi2
+rewriteExpression2 f = process' phi where
+	atom err a ff = ff f <$> processAtom err a f
+	phi expr = case expr of
+		Constant _ -> Nothing
+		Atom a -> do
+			([], e) <- processAtom "Lone" a f
+			return e
+		Application (Atom a) b -> atom "SingleApp" a $ rewriteAtomApplication b
+		Application (Application (Atom a) b) c -> atom "DoubleApp" a $ rewriteDoubleAtomApplication b c
+		Application _ _ -> Nothing
 
-phi2 :: FactBase ListFact -> Rewrite ExpressionFix
-phi2 _ (Constant _) = Nothing
-phi2 f a @ (Atom aa) = do
-	([], e) <- processAtom "rewriteExpression" aa $ xtrace ("factBase-atom {" ++ show a ++ "}") f
-	return e
-phi2 f (Application (Atom a) b') = case processAtom "rewriteApplication.Single" a f of
-	Nothing -> Nothing
-	Just (args, expr) -> Just $ case args of
+rewriteAtomApplication b' f (args, expr) = case args of
 		[] -> Application expr b'
-		_ -> dropR (deep $ rewriteExpression2 (flip mapUnion f $ mapFromList $ zip args $ map (PElem . LetNode []) b')) expr -- inlineApplication args b' f expr
-phi2 f xx | isDoubleAtomApplication xx = rewriteDoubleAtomApplication f xx
-phi2 _ (Application _ _) = Nothing
+		_ -> dropR (deep $ rewriteExpression2 (flip mapUnion f $ mapFromList $ zip args $ map (PElem . LetNode []) b')) expr
