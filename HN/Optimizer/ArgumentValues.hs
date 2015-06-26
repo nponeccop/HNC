7a13663b957e487a5b02505fa8e168b5634e6121
Andrii Melnykov <andy.melnikov@gmail.com>
2015-06-26 04:14:20 +0300

    New: PoC unzipping of LetNode arguments

--- HN/Optimizer/ArgumentValues.hs
+++ HN/Optimizer/ArgumentValues.hs
@@ -9,6 +9,7 @@ import Data.Functor.Foldable hiding (Fix)
 import HN.Intermediate
 import HN.Optimizer.Lattice
 import HN.Optimizer.Node
+import HN.Optimizer.Pass
 
 type ArgFact = WithTopAndBot [WithTopAndBot ExpressionFix]
 
@@ -33,19 +34,26 @@ process = unzippedPara $ \f s -> F.concat s ++ varArgs f
 
 transferF = mkFTransfer ft where
 	ft :: Node e x -> ArgFact -> Fact x ArgFact
-	ft (Entry _) _ = undefined
-	ft (Exit dn) _ = mkFactBase argLattice $ map (second $ PElem . map PElem) $ case dn of
-		LetNode _ v -> process v
+	ft (Entry _) f = f
+	ft (Exit dn) f = mkFactBase argLattice $ map (second $ PElem . map PElem) $ case dn of
+		LetNode args value -> unzipArgs f args ++ process value
 		ArgNode -> []
 		LibNode -> []
 
-avPass :: FwdPass
-          SimpleFuelMonad
-          HN.Optimizer.Node.Node
-          (WithTopAndBot [WithTopAndBot HN.Optimizer.Node.ExpressionFix])
+unzipArgs :: ArgFact -> [Label] -> [(Label, [ExpressionFix])]
+unzipArgs (PElem actualArgs) formalArgs
+	| length actualArgs /= length formalArgs = error "Wrong formalArgs"
+	| otherwise = concatMap foo $ zip formalArgs actualArgs
+unzipArgs _ _ = []
 
+foo (formalArg, PElem actualArg) = [(formalArg, [actualArg])]
+foo _ = []
+
+avPass :: FwdPass SimpleFuelMonad Node ArgFact
 avPass = FwdPass 
 	{ fp_lattice = argLattice
 	, fp_transfer = transferF
 	, fp_rewrite = noFwdRewrite
 	}
+
+runAv = runPass (analyzeAndRewriteFwd avPass) (\_ _ -> mapEmpty)
