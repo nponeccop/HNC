cec9ad80455144e280f6acad366cb8c8d4079b52
Andrii Melnykov <andy.melnikov@gmail.com>
2015-06-30 19:46:54 +0300

    Refactor: cleaned GraphDecompiler

--- HN/Optimizer/GraphDecompiler.hs
+++ HN/Optimizer/GraphDecompiler.hs
@@ -2,25 +2,25 @@
 module HN.Optimizer.GraphDecompiler (decompileGraph) where
 
 import Compiler.Hoopl
-import HN.Optimizer.Visualise ()
 import qualified Data.Map as M
-import qualified HN.Optimizer.Node as N
+import Data.Maybe
+import Safe (fromJustNote)
+
 import HN.Intermediate
 import HN.Optimizer.Dominator
-import Data.Maybe
+import qualified HN.Optimizer.Node as N
 import Utils
 
 firstLabel = runSimpleUniqueMonad freshLabel
 
 decompileGraph :: M.Map Label String -> Graph N.Node C C -> Definition String
-decompileGraph labelNames g = l2n <$> fromJust (decompiledGraph l2pd g firstLabel) where
-	l2n l = uncondLookup l labelNames
-	l2pd l = M.findWithDefault [] l $ graphPostdominators g
+decompileGraph labelNames g = fmap (`uncondLookup` labelNames ) . fromJust
+	$ lookupLet (flip (M.findWithDefault []) $ graphPostdominators g) (lookupValue g) firstLabel
 
-decompiledGraph l2pd g l = insertLet (mapMaybe (decompiledGraph l2pd g) $ l2pd l) <$> byLabel l where
-	byLabel = lookupDefinition g
+lookupLet l2pd l2value = self where
+	self l = insertLet (mapMaybe self $ l2pd l) <$> l2value l
 
-mapUncond k m = fromMaybe (error "mapUncond failed in GraphDecompiler") $ mapLookup k m
+mapUncond k m = fromJustNote "GraphDecompiler.mapUncond" $ mapLookup k m
 
 decompiledBlock :: Block N.Node C C -> N.DefinitionNode
 decompiledBlock x = foldBlockNodesB f x undefined where
@@ -28,7 +28,7 @@ decompiledBlock x = foldBlockNodesB f x undefined where
 	f (N.Entry _) o = o
 	f (N.Exit d) _ = d
 
-lookupDefinition :: Graph N.Node e x -> Label -> Maybe (Definition Label)
-lookupDefinition (GMany _ l _) x = case decompiledBlock $ mapUncond x l of
+lookupValue :: Graph N.Node e x -> Label -> Maybe (Definition Label)
+lookupValue (GMany _ l _) x = case decompiledBlock $ mapUncond x l of
 	N.LetNode argLabels expr -> Just $ Definition x argLabels (In expr)
 	_ -> Nothing
