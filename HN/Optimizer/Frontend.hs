84d683c2429b735aaa5252507fefc7809756f19d
Andrii Melnykov <andy.melnikov@gmail.com>
2015-07-04 21:12:46 +0300

    Internal: disabled noInliner and newInliner passes

--- HN/Optimizer/Frontend.hs
+++ HN/Optimizer/Frontend.hs
@@ -5,18 +5,30 @@ import HN.Optimizer.GraphCompiler
 import HN.Optimizer.GraphDecompiler
 import HN.Optimizer.Inbound (runF)
 import HN.Optimizer.Inliner2 (runB)
-import qualified HN.Optimizer.ArgumentValues as AV
 import qualified HN.Optimizer.FormalArgumentsDeleter as AAD
+import qualified HN.Optimizer.ArgumentValues as AV
+import qualified HN.Optimizer.ArgumentDeleter as AD
+
+import HN.Optimizer.Node (Pass)
 import Utils
+import Debug.Trace
+import HN.Optimizer.Visualise
 
-optimizeHN libraryTypes = map (withGraph libraryTypes (fromTuple . runSimpleUniqueMonad . runWithFuel infiniteFuel . (runFB >=> runFB) . toTuple))
+optimizeHN libraryTypes = map (withGraph libraryTypes (fromTuple . runSimpleUniqueMonad . runWithFuel infiniteFuel . passes . toTuple))
 
-runFB = runF >=> runB
+oldInliner = runF >=> runB
 
-oo = AV.runAv >=> AAD.runB
+newInliner = AV.runAv >=> AD.runF >=> AAD.runB
+
+passes = oldInliner -- >=> oldInliner >=> oldInliner --AV.runAv >=> AD.runF-- >=> AAD.runB
+
+noInliner :: Pass any ()
+noInliner (x, _, _) = return (x, noFacts, undefined)
+
+otrace _ x = x
 
 toTuple agraph = (agraph, undefined, undefined)
-fromTuple (agraph, _, _) = agraph
+fromTuple (agraph, facts, _) = otrace (formatGraph agraph ++ show facts) agraph
 
 withGraph libNames f def = decompileGraph (xtrace "labelNames" labelNames) $ f graph where
 	(graph, (_, labelNames)) = compileGraph libNames def
