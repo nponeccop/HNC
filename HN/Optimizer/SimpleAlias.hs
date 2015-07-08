fab734e74757062845027bb8e50c12a8c1a58312
Andrii Melnykov <andy.melnikov@gmail.com>
2015-07-08 22:00:49 +0300

    New: simple aliases are inlined
    Partial fix for #51

---
+++ HN/Optimizer/SimpleAlias.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleInstances #-}
+module HN.Optimizer.SimpleAlias (runB) where
+
+import Compiler.Hoopl
+import Safe.Exact
+
+import HN.Intermediate
+import HN.Optimizer.ClassyLattice
+import HN.Optimizer.ExpressionRewriter
+import HN.Optimizer.Lattice
+import HN.Optimizer.Node
+import HN.Optimizer.Utils
+import HN.Optimizer.Pass
+
+type SAFact = WithTopAndBot ExpressionFix 
+
+instance Lattice SAFact where
+	dataflowLattice = flatEqLattice "SimpleAlias" 
+
+transferB :: DefinitionNode -> FactBase SAFact -> SAFact
+transferB (LetNode args (Application aa @ (Atom _) argValues)) _ = pelemJust $ fmap (const aa) . sequence =<< zipWithExactMay f args argValues where
+	f a (Atom b) | a == b = Just ()
+	f _ _ = Nothing
+transferB _ _ = bot
+
+rewriteB :: DefinitionNode -> FactBase SAFact -> Maybe DefinitionNode
+rewriteB (LetNode l expr) f = LetNode l <$> process (rewriteExpression f) expr
+rewriteB _ _ = Nothing
+
+rewriteExpression :: FactBase SAFact -> Rewrite ExpressionFix
+rewriteExpression f (Atom a) = justPElem =<< lookupFact a f
+rewriteExpression _ _ = Nothing
+
+justPElem (PElem a) = Just a
+justPElem _ = Nothing
+
+pelemJust = maybe Bot PElem
+
+runB :: Pass any SAFact
+runB = runPassB PassParams
+	{ ppConvertFacts = \_ _ -> noFacts
+	, ppTransfer = mkBTransfer $ transferExitB transferB
+	, ppRewrite = pureBRewrite $ rewriteExitB rewriteB
+	}
+
