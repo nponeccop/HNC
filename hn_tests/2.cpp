31694da61f88a05ebef9a0f3483e516fe291db9b
Andrii Melnykov <andy.melnikov@gmail.com>
2016-11-04 07:58:42 +0200

    Internal: more tests
    Added some untracked files from my local tree

---
+++ hn_tests/2.cpp
@@ -0,0 +1,8 @@
+#include <hn/lib.hpp>
+
+template <typename t0>
+t0 a(t0 x)
+{
+	t0 b = x;
+	return b;
+};
---
+++ hn_tests/opt/2.cpp
@@ -0,0 +1,7 @@
+#include <hn/lib.hpp>
+
+template <typename t0>
+t0 a(t0 x)
+{
+	return x;
+};
