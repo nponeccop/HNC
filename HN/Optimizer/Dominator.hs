0eaaeea561160037712c16cac25507b1cef6e200
Denis Stoyanov <stoyanov.gr@gmail.com>
2017-03-17 04:44:39 +0200

    Has been refactore Dominator.hs

--- HN/Optimizer/Dominator.hs
+++ HN/Optimizer/Dominator.hs
@@ -1,22 +1,26 @@
 module HN.Optimizer.Dominator where
+
 import Compiler.Hoopl
 import Compiler.Hoopl.Passes.Dominator
 import qualified Data.Map as M
-import HN.Optimizer.Pass
 import HN.Optimizer.Node
+import HN.Optimizer.Pass
 
-type Foo n f t1 t2
-	= (Graph n C C, t1, t2) 
-	-> SimpleFuelMonad (Graph n C C, FactBase f, MaybeO C f) 
+type Foo n f t1 t2 = (Graph n C C, t1, t2) -> SimpleFuelMonad (Graph n C C, FactBase f, MaybeO C f)
+
 runDominatorF :: Foo Node Doms t1 t2
-runDominatorF = runPass (analyzeAndRewriteFwd domPass) (\_ entry -> mapSingleton entry domEntry)
+runDominatorF = runPass (analyzeAndRewriteFwd domPass) (const (`mapSingleton` domEntry))
 
-graphPostdominators g = runSimpleUniqueMonad $ runWithFuel infiniteFuel $
-	runDominatorF (g, undefined, undefined) >>= \(_, f, _) -> return $ immediatePostdominators $ immediateDominators f
+graphPostdominators :: Graph Node C C -> M.Map Label [Label]
+graphPostdominators g =
+  runSimpleUniqueMonad $
+  runWithFuel infiniteFuel $
+  runDominatorF (g, undefined, undefined) >>= \(_, f, _) -> return $ immediatePostdominators $ immediateDominators f
 
 immediatePostdominators :: LabelMap Label -> M.Map Label [Label]
-immediatePostdominators l = xx where
-	ll = mapToList l
-	xx :: M.Map Label [Label]
-	xx = foldr f M.empty ll
-	f (k, v) =  M.insertWith (\_ o -> k : o) v [k]
+immediatePostdominators l = xx
+  where
+    ll = mapToList l
+    xx :: M.Map Label [Label]
+    xx = foldr f M.empty ll
+    f (k, v) = M.insertWith (\_ o -> k : o) v [k]
